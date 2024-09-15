dbeb0a12a6621060ab503da54a3df4d1d21a65d5
Silv <91438536+Polsiv@users.noreply.github.com>
2024-09-15 13:06:52 -0500

    Update htbmachines.sh

--- htbmachines.sh
+++ htbmachines.sh
@@ -25,8 +25,11 @@ main_url="https://htbmachines.github.io/bundle.js"
 #help panel
 function helpPanel(){
     echo -e "\n ${yellowColour}[+] ${endColour} ${grayColour}Usage: ${grayColour}"
-    echo -e "\t ${purpleColour} u) ${endColour} ${grayColour} Update ${endColour}"
+    echo -e "\t ${purpleColour} d) ${endColour} ${grayColour} Search by Difficulty ${endColour}\n"
     echo -e "\t ${purpleColour} m) ${endColour} ${grayColour} Search by machine name ${endColour}"
+    echo -e "\t ${purpleColour} i) ${endColour} ${grayColour} Search by IP address ${endColour}" 
+    echo -e "\t ${purpleColour} u) ${endColour} ${grayColour} Update ${endColour}"
+    echo -e "\t ${purpleColour} y) ${endColour} ${grayColour} Display YT link${endColour}" 
     echo -e "\t ${purpleColour} h) ${endColour} ${grayColour} Help Panel ${endColour}\n"
 }
 
@@ -72,21 +75,63 @@ function updateFiles(){
 
 #searchMachine
 function searchMachine(){
+    
     machineName="$1"
-    echo -e "$machineName"
+    
+    echo -e "\n ${yellowColour}[+] ${endColour} ${grayColour} Machine Properties: ${endColour} \n"
+
+    cat bundle.js | awk "/name: \"$machineName\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta" | tr -d '"' | tr  -d ',' | sed 's/^ *//'  | awk '{printf "\033[0;37m%s\033[0m \033[0;36m%s\033[0m\n", $1, $2}'
+    
 }
 
+
+#searchip
+function searchIP(){
+    
+    machineip="$1"
+    machine_name="$(cat bundle.js | grep "ip: \"$machineip\"" -B 3 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',')"
+    echo -e "\n ${yellowColour}[+] ${endColour} ${grayColour}Respective machine for ${endColour} ${blueColour} $machineip ${endColour} ${grayColour}is: ${endColour}"
+    searchMachine $machine_name
+
+}
+
+#search diff
+function searchDifficulty(){
+    diff="$1"
+    check="$(cat bundle.js | grep "dificultad: \"$diff\"" -B 5 | grep "name" | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column)"
+    if [ "$check" ]; then
+        cat bundle.js | grep "dificultad: \"$diff\"" -B 5 | grep "name" | awk 'NF{print $NF}' | tr -d '"' | tr -d ',' | column
+    else
+        echo -e "\n${redColour}[!] Difficulty not found lol! ${endColour}\n"    
+    fi 
+
+}
+
+#obtain Link
+function obtainLink(){
+    machineName="$1"
+    check="$(cat bundle.js | awk "/name: \"$machineName\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta" | tr -d '"' | tr  -d ',' | sed 's/^ *//'  | grep "youtube" | awk 'NF{print $NF}')"
+
+    if [ "$check" ]; then
+        echo -e "\n ${yellowColour}[+]${endColour} ${grayColour}link: $check ${endColour}"    
+    else
+        echo -e "\n${redColour}[!]${endColour} ${grayColour}Not found! ${endColour}"
+    fi
+}
 
 #Indicators 
 declare -i parameter_counter=0 # -i indicates integer
 
 #Menu
-while getopts "m:hu" arg; do
+while getopts "m:hi:y:d:u" arg; do
 
     case $arg in
-        m) machineName=$OPTARG; let parameter_counter+=1;;
+        m) machineName="$OPTARG"; let parameter_counter+=1;;
         u) let parameter_counter+=2;;
         h) helpPanel;;
+        i) machineIP="$OPTARG"; let parameter_counter+=3;;
+        y) machineName="$OPTARG"; let parameter_counter+=4;;
+        d) diff="$OPTARG"; let parameter_counter+=5;;
     esac
 done
 
@@ -96,6 +141,17 @@ if [ $parameter_counter -eq 1 ]; then
 
 elif [ $parameter_counter -eq 2 ]; then
     updateFiles
+
+elif [ $parameter_counter -eq 3 ]; then
+    searchIP $machineIP
+
+elif [ $parameter_counter -eq 4 ]; then
+    obtainLink $machineName 
+
+elif [ $parameter_counter -eq 5 ]; then
+    searchDifficulty $diff
+
 else
     helpPanel
 fi
+
