eb36122165e3fcb5a8a9a5a9d3be8233a4a70218
polsiv <forenks1005@gmail.com>
2024-09-17 00:28:25 -0500

    s

--- bash scripts/casino.sh
+++ bash scripts/casino.sh
@@ -11,7 +11,7 @@ turquoiseColor="\e[0;36m\033[1m"
 grayColor="\e[0;37m\033[1m"
 
 function ctrl_c(){
-    echo -e  "\n ${redColor}[!] Exiting... ${endColour}"
+    echo -e  "\n ${redColor}[!] Exiting... ${endColor}"
     tput cnorm; exit 1
 }   
 
@@ -20,7 +20,6 @@ function helpPanel(){
     echo -e "\t ${blueColor}-m)  ${endColor}${grayColor}Money ${endColor}"
     echo -e "\t ${blueColor}-t)  ${endColor}${grayColor}Technique (Martingala, Inverselabrouchere)${endColor} "
     echo -e "\t ${blueColor}-h)  ${endColor}${grayColor}Help ${endColor}\n"
-
 }
 
 
@@ -29,11 +28,51 @@ trap ctrl_c INT
 
 
 function martingala(){
-    echo -e "\n[+] current money: $1"
-    echo -n "[+] Money to bet: " && read initial_bet
-    echo -n "[+] Choose wisely (even/odd)" && read even_odd
 
-    echo 
+    tput civis
+    echo -e "\n${yellowColor}[+] ${endColor} ${grayColor} current money: ${grayColor} ${blueColor}$1 ${endColor}"
+    echo -ne "${yellowColor}[+] ${endColor}${grayColor}Money to bet: ${grayColor}" && read initial_bet
+    echo -ne "${yellowColor}[+] ${endColor}${grayColor}${grayColor} Choose wisely (even/odd): ${endColor}" && read even_odd
+
+    backup_bet=$initial_bet
+
+    while true; do
+
+
+
+        money=$(($money-$initial_bet))
+        echo -e "\n[+] $initial_bet on the line! and this yo money: $money"
+        rand_number="$(($RANDOM % 37))"
+        echo -e "${grayColor}[+] Number rolled: $rand_number${endColor}"
+        
+        if [ ! $money -le 0 ]; then
+            if [ $even_odd == "even" ]; then
+                if [ "$(($rand_number % 2))" -eq 0 ]; then
+                    if [ $rand_number -eq 0 ]; then
+                        echo -e "${redColor}[-] 0 rolled, YOU LOSE!${endColor}"
+                        initial_bet=$(($initial_bet * 2))
+                        echo -e "${grayColor}[+] Here's yo Money: $money ${endColor}"
+                    else
+                        reward=$(($initial_bet*2))
+                        echo -e "${greenColor}[+] Even, YOU WIN! ${endColor}"
+                        echo -e "${grayColor}[+] Heres the reward: ${endColor}${greenColor} $reward${endColor}"
+                        money=$(($money+$reward))
+                        echo -e "${grayColor}[+] Here's yo Money: $money ${endColor}"
+                        initial_bet=$backup_bet
+                    fi
+                else
+                    echo -e "${redColor}[-] Odd, YOU LOSE!${endColor}"
+                    initial_bet=$(($initial_bet * 2))
+                    echo -e "${grayColor}[+] Here's yo Money: $money ${endColor}"
+                fi
+            fi
+        else
+            echo -e "\n${redColor}[!] You ran out of money my G!${endColor}\n"
+            tput cnorm; exit 0 
+        fi
+        sleep 0.5
+    done
+    
 }
 
 while getopts "m:t:h" args; do
@@ -45,7 +84,6 @@ while getopts "m:t:h" args; do
 
     esac
 done
-
 
 
 if [ $money ] && [ $technique ]; then
